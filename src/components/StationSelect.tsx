d382c75cfc1d4bb012333ec5d0ea7c77fce42cbb
Patryk-Kuchta <patryk.kuchta@softwire.com>
2024-11-12 12:24:46 +0000

    add selecting both and redirection

---
+++ src/components/StationSelect.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from 'react';
+import hardCodedStations from '../data/hardCodedStations.json';
+
+type StationSelectInput = {
+    label: string;
+    invalidSelections: string[];
+    setSelection: (crs: string) => void;
+}
+
+const StationSelect : FC<StationSelectInput> = ({ label, invalidSelections, setSelection }) => {
+    const elementId = label.toLowerCase();
+
+    return <>
+        <label htmlFor = { elementId }>{label} station:</label>
+        <select
+            name = { elementId }
+            id = { elementId }
+            onChange = { (event) => {
+                setSelection(event.target.value);} }
+        >
+            {
+                hardCodedStations.map((station, key) => {
+                    return (
+                        <option
+                            value = { station.crs }
+                            key = { key }
+                            disabled = { invalidSelections.includes(station.crs) }
+                        >
+                            {station.name}
+                        </option>);
+                },
+                )
+            }
+        </select>
+    </>;
+};
+
+export default StationSelect;
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,22 +1,31 @@
-import React from 'react';
-import hardCodedStations from '../data/hardCodedStations.json';
+import React, { useState } from 'react';
+import StationSelect from '../components/StationSelect';
 
 const SearchPage: React.FC = () => {
+
+    const [originStation, setOriginStation] = useState('hi!!!!!!');
+    const [destinationStation, setDestinationStation] = useState('helo');
+
+    const performSearch = () => {
+        window.location.href = `https://www.lner.co.uk/travel-information/travelling-now/live-train-times/depart/${originStation}/${destinationStation}/#LiveDepResults`;
+    };
 
     return (
         <div>
             <h1>Find you next journey...</h1>
-            <label htmlFor = { 'origin' }>Origin station:</label>
-            <select name = { 'origin' } id = { 'origin' }>
-                {
-                    hardCodedStations.map((station, key) => {
-                        return <option value = { station.crs } key = { key }>
-                            {station.name}
-                        </option>;
-                    },
-                    )
-                }
-            </select>
+            <StationSelect
+                label = { 'Origin' }
+                invalidSelections = { [destinationStation] }
+                setSelection = { setOriginStation }
+            />
+            <StationSelect
+                label = { 'Destination' }
+                invalidSelections = { [originStation] }
+                setSelection = { setDestinationStation }
+            />
+            <button type = { 'submit' } onClick = { performSearch }>
+                Search...
+            </button>
         </div>
     );
 };
