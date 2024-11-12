f0ba64addd911b34f5ecf6e47f74ca5ddc3c2261
Patryk-Kuchta <patryk.kuchta@softwire.com>
2024-11-12 12:05:20 +0000

    add a simple origin select

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import hardCodedStations from '../data/hardCodedStations.json';
 
 const SearchPage: React.FC = () => {
 
     return (
         <div>
-            Find you next journey...
+            <h1>Find you next journey...</h1>
+            <label htmlFor = { 'origin' }>Origin station:</label>
+            <select name = { 'origin' } id = { 'origin' }>
+                {
+                    hardCodedStations.map((station, key) => {
+                        return <option value = { station.crs } key = { key }>
+                            {station.name}
+                        </option>;
+                    },
+                    )
+                }
+            </select>
         </div>
     );
 };
