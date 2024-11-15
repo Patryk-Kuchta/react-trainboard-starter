c415dbeeed0c3a6113f9341130712feec6e9cc7a
Patryk-Kuchta <patryk.kuchta@softwire.com>
2024-11-15 14:53:42 +0000

    improve the icons in the results

--- src/components/JourneyDisplay.tsx
+++ src/components/JourneyDisplay.tsx
@@ -1,14 +1,17 @@
-import React, { FC } from 'react';
+import React, { FC, ReactNode } from 'react';
+import { ImCancelCircle } from 'react-icons/im';
+import { IoCheckboxOutline, IoPlayForwardSharp, IoTimerOutline } from 'react-icons/io5';
+import { MdGroupOff } from 'react-icons/md';
 import moment from 'moment/moment';
 import { DepartureInfo, Status } from '../pages/SearchPage';
 
 const JourneyDisplay : FC<{journey: DepartureInfo}> = ({ journey }) => {
 
-    const statusToEmoji: { [key in Status]: string } = {
-        normal: '✅',
-        delayed: '⏳',
-        cancelled: '❌',
-        fully_reserved: '🚫',
+    const statusToEmoji: { [key in Status]: ReactNode } = {
+        normal: <><IoCheckboxOutline /> normal. </>,
+        delayed: <><IoTimerOutline /> This train is <b>delayed</b>. </>,
+        cancelled: <><ImCancelCircle /> This train is <b>cancelled</b>. </>,
+        fully_reserved: <><MdGroupOff /> This train is <b>sold out</b>. </>,
     };
 
     const arrival = moment(journey.arrivalTime);
@@ -16,21 +19,27 @@ const JourneyDisplay : FC<{journey: DepartureInfo}> = ({ journey }) => {
 
     return (
         <div
-            className = { 'journeyDisplay' }
+            className = { 'journey_display' }
         >
-            {departure.format('HH:mm')}
+            <div className = { 'datetime_container' }>
+                {departure.format('HH:mm')}
 
-            <span className = { 'date' }> {departure.format('DD MMM YY')}</span>
+                <span className = { 'date' }> {departure.format('DD MMM YY')}</span>
+            </div>
 
-            ➡️
+            <IoPlayForwardSharp />
 
-            {arrival.format('HH:mm')}
+            <div className = { 'datetime_container' }>
+                {arrival.format('HH:mm')}
 
-            <span className = { 'date' }> {departure.format('DD MMM YY')}</span>
+                <span className = { 'date' }> {departure.format('DD MMM YY')}</span>
+            </div>
 
-            <hr/>
-
-            Status: {statusToEmoji[journey.status]}
+            {journey.status !== 'normal' &&
+                <>
+                    <hr/>
+                    {statusToEmoji[journey.status]}
+                </>}
 
             <hr/>
 
